0e7c1492c71b9a43074a7a27d01b8d7b992f404c
Harjot Singh <harjot@harkul.com>
2018-03-23 17:45:59 +0000

    utils: reorganise and refactor into `toUnicode` function

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-/**
- * Converts ASCII text used in the GurmukhiAkhar font to Unicode.
- * @param text The ascii text to convert
- */
-const akharToUnicode = text => text
-  .replace( /i./g, match => match.split( '' ).reverse().join( '' ) )
-  .split( '' )
-  .map( c => ( unicodeMappings[ c ] !== undefined ? unicodeMappings[ c ] : c ) )
-  .join( '' )
-  .replace( /ਿ੍./g, match => match.slice( 1, 3 ).concat( 'ਿ' ) )
+const toUnicode = require( './lib/toUnicode' )
 
-
-module.exports = akharToUnicode
+module.exports = {
+  toUnicode
+}
