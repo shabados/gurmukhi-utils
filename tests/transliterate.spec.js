48ee419ce6101549cbb0c5f2ae9192f7e2be7fc4
Harjot Singh <harjot@harkul.com>
2018-06-15 20:40:16 +0100

    Add more tests

--- tests/transliterate.spec.js
+++ tests/transliterate.spec.js
@@ -5,7 +5,8 @@ const { transliterate } = require( '../index' )
 // Test lines
 const transliterations = [
   [ 'ikv sicAwrw hoeIAY ikv kUVY qutY pwil ]', "kiv sachiaaraa hoe'eeaai kiv kooRai tuTai paal ||" ],
-  // [ 'ਕਿਵ ਸਚਿਆਰਾ ਹੋਈਐ ਕਿਵ ਕੂੜੈ ਤੁਟੈ ਪਾਲਿ ॥', 'kiv sachiaaraa hoeeeaai kiv kooRai tuTai paal ||' ],
+  [ 'knkM duiq aujl AMg sny ]', 'kanakan dhut aujal ang sane ||' ],
+  [ 'gun imRghw ko icq bIc lIAw ]385]', 'gun mrigahaa ko chit beech leeaa ||385||' ],
 ]
 
 describe( 'transliterate()', () => {
