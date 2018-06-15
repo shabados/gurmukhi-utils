a8eeb9e49496331a6af19252d14175ebf944b4f7
Harjot Singh <harjot@harkul.com>
2018-06-15 21:08:37 +0100

    Add more tests to transliterator

--- tests/transliterate.spec.js
+++ tests/transliterate.spec.js
@@ -7,6 +7,9 @@ const transliterations = [
   [ 'ikv sicAwrw hoeIAY ikv kUVY qutY pwil ]', "kiv sachiaaraa hoe'eeaai kiv kooRai tuTai paal ||" ],
   [ 'knkM duiq aujl AMg sny ]', 'kanakan dhut aujal ang sane ||' ],
   [ 'gun imRghw ko icq bIc lIAw ]385]', 'gun mrigahaa ko chit beech leeaa ||385||' ],
+  [ 'gx gMDRb BUq ipswc qbY ]388]', 'gaN gandhrab bhoot pisaach tabai ||388||' ],
+  [ 'AbRXkq AMg ]', 'abrayakat ang ||' ],
+  [ 'jgMnwQ jgdIsur krqy sB vsgiq hY hir kyrI ]', 'jagannaath jagadheesur karate sabh vasagat hai har keree ||' ],
 ]
 
 describe( 'transliterate()', () => {
