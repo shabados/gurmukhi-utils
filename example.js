f75a16992cd20fd7352566f3ef316a9720793d61
Harjot Singh <harjot@harkul.com>
2018-06-18 00:28:41 +0100

    Add custom runkit example

---
+++ example.js
@@ -0,0 +1,6 @@
+const { toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
+
+toUnicode( 'Koj' )
+firstLetters( 'hir hir hir gunI' )
+firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' )
+transliterate( 'hukmI hukmu clwey rwhu ]' )
