1651d554fdb12d05b748124f221127d8d2feca0a
Harjot Singh <harjot@harkul.com>
2018-06-18 00:39:49 +0100

    Update runkit example. #Patch release.

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const { toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
 
-toUnicode( 'Koj' )
-firstLetters( 'hir hir hir gun gwvhu ]' )
-firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' )
-transliterate( 'hukmI hukmu clwey rwhu ]' )
+console.log(toUnicode( 'Koj' ))
+console.log(firstLetters( 'hir hir hir gun gwvhu ]' ))
+console.log(firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
+console.log(transliterate( 'hukmI hukmu clwey rwhu ]' ))
