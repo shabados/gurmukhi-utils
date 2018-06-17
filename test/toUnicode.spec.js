659aacf242abc5e05dab52553a1833eefa7f70c4
Harjot Singh <harjot@harkul.com>
2018-06-17 22:30:24 +0100

    Add additional tests

--- test/toUnicode.spec.js
+++ test/toUnicode.spec.js
@@ -7,6 +7,10 @@ const words = [
   [ 'Koj', 'ਖੋਜ' ],
   [ 'ihr', 'ਹਿਰ' ],
   [ 'imil´o', 'ਮਿਲੵੋਿ' ],
+  [ 'qU pRB dwqw dwin miq pUrw hm Qwry ByKwrI jIau ]', 'ਤੂ ਪ੍ਰਭ ਦਾਤਾ ਦਾਨਿ ਮਤਿ ਪੂਰਾ ਹਮ ਥਾਰੇ ਭੇਖਾਰੀ ਜੀਉ ॥' ],
+  [ 'so bRhmu AjonI hY BI honI Gt BIqir dyKu murwrI jIau ]2]', 'ਸੋ ਬ੍ਰਹਮੁ ਅਜੋਨੀ ਹੈ ਭੀ ਹੋਨੀ ਘਟ ਭੀਤਰਿ ਦੇਖੁ ਮੁਰਾਰੀ ਜੀਉ ॥੨॥' ],
+  [ 'zny pyc dsqwr rw qwbdwd ]', 'ਜ਼ਨੇ ਪੇਚ ਦਸਤਾਰ ਰਾ ਤਾਬਦਾਦ ॥' ],
+
 ]
 
 describe( 'toUnicode()', () => {
