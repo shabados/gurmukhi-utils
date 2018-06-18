35cb37977d419f0a8816c4dc1c7ae6df3dc9d3b4
Harjot Singh <harjot@harkul.com>
2018-06-18 02:44:47 +0100

    Add example usage in docs

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
-const { toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
+const { toAscii, toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
 
 console.log(toUnicode( 'Koj' ))
+console.log(toAscii('ਖੋਜ'))
 console.log(firstLetters( 'hir hir hir gun gwvhu ]' ))
 console.log(firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
 console.log(transliterate( 'hukmI hukmu clwey rwhu ]' ))
