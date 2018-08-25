a83d45b91023303b10c60ca26058ed33afde7420
Harjot Singh <harjot@harkul.com>
2018-08-25 01:35:02 +0100

    Add Hindi to RunKit example #Patch

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-const { toShahmukhi, toAscii, toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
+const { toHindi, toShahmukhi, toAscii, toUnicode, firstLetters, transliterate } = require( 'gurmukhi-utils' )
 
 console.log(toUnicode( 'Koj' ))
 console.log(toAscii('ਖੋਜ'))
@@ -6,3 +6,4 @@ console.log(firstLetters( 'hir hir hir gun gwvhu ]' ))
 console.log(firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
 console.log(transliterate( 'hukmI hukmu clwey rwhu ]' ))
 console.log(toShahmukhi( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
+console.log(toHindi( 'ਕੁਲ ਜਨ ਮਧੇ ਮਿਲੵੋਿ ਸਾਰਗ ਪਾਨ ਰੇ ॥' ))
