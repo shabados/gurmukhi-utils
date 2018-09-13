483811bc4bb094e33602f1e041c53b7094529529
Harjot Singh <harjot@harkul.com>
2018-09-13 08:19:22 +0100

    Add TS typings
    Fixes #17. #Patch.

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export function toUnicode(text: string): string
+
+export function toAscii(text: string): string
+
+export function toEnglish(text: string): string
+
+export function toHindi(text: string): string
+
+export function toShamukhi(text: string): string
+
+export function firstLetters(text: string, stripNukta?: boolean = true): string
