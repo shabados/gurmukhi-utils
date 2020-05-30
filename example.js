bbbc8d7368e92bb606f81249ed51d7e88cb598c8
Sarabveer Singh <me@sarabveer.me>
2020-05-30 17:36:13 -0400

    docs: update example.js and example in README

--- example.js
+++ example.js
@@ -4,16 +4,19 @@ const {
   toAscii,
   toUnicode,
   firstLetters,
-  transliterate,
+  toEnglish,
   stripAccents,
+  stripVishraams,
+  isGurmukhi
 } = require( 'gurmukhi-utils' )
 
 console.log(toUnicode( 'Koj' ))
 console.log(toAscii('ਖੋਜ'))
 console.log(firstLetters( 'hir hir hir gun gwvhu ]' ))
 console.log(firstLetters( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
-console.log(transliterate( 'ਹੁਕਮੀ ਹੁਕਮੁ ਚਲਾਏ ਰਾਹੁ ॥' ))
+console.log(toEnglish( 'ਹੁਕਮੀ ਹੁਕਮੁ ਚਲਾਏ ਰਾਹੁ ॥' ))
 console.log(toShahmukhi( 'ਹਰਿ ਹਰਿ ਹਰਿ ਗੁਨੀ' ))
 console.log(toHindi( 'ਕੁਲ ਜਨ ਮਧੇ ਮਿਲੵੋਿ ਸਾਰਗ ਪਾਨ ਰੇ ॥' ))
 console.log(stripAccents('ਜ਼ਫ਼ੈਸ਼ਸ'))
 console.log(stripVishraams('sbid mrY. so mir rhY; iPir.'))
+console.log(isGurmukhi('ਗੁਰਮੁਖੀ'))
