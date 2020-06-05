ef2e1e654bc6407a84da8d63e0c036a83728b7d9
Harjot Singh <harjot@harkul.com>
2020-06-05 19:43:20 +0100

    fix(firstLetters): remove spaces from firstLetter input strings
    Prevents undefined being outputted in the resultant string

--- test/firstLetters.spec.js
+++ test/firstLetters.spec.js
@@ -1,14 +1,16 @@
 const { expect } = require( 'chai' )
 
-const { firstLetters, toHindi, toEnglish } = require( '../index' )
+const { firstLetters, toUnicode, toHindi, toEnglish } = require( '../index' )
 
 describe( 'firstLetters(gurmukhi)', () => {
   const lines = [
+    [ 'ਗੁਰਮੁਖਿ  ਲਾਧਾ ਮਨਮੁਖਿ   ਗਵਾਇਆ ॥', 'ਗਲਮਗ॥' ],
     [ 'ਗੁਰਮੁਖਿ ਲਾਧਾ ਮਨਮੁਖਿ ਗਵਾਇਆ ॥', 'ਗਲਮਗ॥' ],
     [ 'ਜਿਨਿ ਹਰਿ ਸੇਵਿਆ ਤਿਨਿ ਸੁਖੁ ਪਾਇਆ ॥', 'ਜਹਸਤਸਪ॥' ],
     [ 'ਗ਼ੈਰਿ ਹਮਦਿ ਹੱਕ ਨਿਆਇਦ ਬਰ ਜ਼ਬਾਨਮ ਹੀਚ ਗਾਹ', 'ਗ਼ਹਹਨਬਜ਼ਹਗ' ],
     [ 'ਸਬਦਿ ਮਰੈ. ਸੋ ਮਰਿ ਰਹੈ; ਫਿਰਿ. ਮਰੈ ਨ, ਦੂਜੀ ਵਾਰ ॥', 'ਸਮ.ਸਮਰ;ਫ.ਮਨ,ਦਵ॥' ],
     [ 'ਇਕਨਾ. ਹੁਕਮੀ ਬਖਸੀਸ; ਇਕਿ, ਹੁਕਮੀ ਸਦਾ ਭਵਾਈਅਹਿ ॥', 'ੲ.ਹਬ;ੲ,ਹਸਭ॥' ],
+    [ toUnicode( 'ik hr hSqo Ssq Awmdw cwkrS [148[' ), 'ਕਹਹਸ਼ਅਚ।' ],
   ]
 
   lines.map( ( [ line, expectedFirstLetters ] ) => it( `should generate first letters for '${line}' as '${expectedFirstLetters}'`, () => {
